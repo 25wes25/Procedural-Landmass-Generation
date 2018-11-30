fe0116cd55ba7faa121383fbc590d30879795582
25wes25 <wesleyswanson97@gmail.com>
2018-11-29 22:26:42 -0800

    Initialized Repository

---
+++ Assets/Editor/MapPreviewEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapPreview))]
+public class MapPreviewEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		MapPreview mapPreview = (MapPreview)target;
+
+		if(DrawDefaultInspector()){
+			if(mapPreview.autoUpdate){
+				mapPreview.DrawMapInEditor();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			mapPreview.DrawMapInEditor();
+		}
+	}
+}
---
+++ Assets/Scripts/HeightMapGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HeightMapGenerator {
+
+	public static HeightMap GenerateHeightMap(int width, int height, HeightMapSettings settings, Vector2 sampleCenter){
+		float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCenter);
+
+		AnimationCurve heightCurve_threadSafe = new AnimationCurve(settings.heightCurve.keys);
+
+		float minValue = float.MinValue;
+		float maxValue = float.MaxValue;
+
+		for (int i = 0; i < width; i++){
+			for (int j = 0; j < height; j++){
+				values[i,j] *= heightCurve_threadSafe.Evaluate(values[i,j]) * settings.heightMultiplier;
+
+				if (values[i,j] > maxValue){
+					maxValue = values[i,j];
+				}
+				if (values[i,j] < minValue){
+					minValue = values[i,j];
+				}
+			}
+		}
+		return new HeightMap(values, minValue, maxValue);
+	}
+
+}
+
+public struct HeightMap{
+	public readonly float[,] values;
+	public readonly float minValue;
+	public readonly float maxValue;
+
+	public HeightMap (float[,] values, float minValue, float maxValue)
+	{
+		this.values = values;
+		this.minValue = minValue;
+		this.maxValue = maxValue;
+	}
+}
